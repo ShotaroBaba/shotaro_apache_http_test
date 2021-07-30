a8f605471c825b8edc9f28fc0840f7572b5b1a37
ShotaroBaba <bshotaro@gmail.com>
2021-07-30 16:51:22 +0200

    Fixed the script adding records to the server.

--- add_value_to_database.sh
+++ add_value_to_database.sh
@@ -1,4 +1,7 @@
-content_name=$(tr -dc a-zA-z < /dev/urandom | head -c 10)
-content_random=$(tr -dc a-zA-z < /dev/urandom | head -c 10)
+content_name=$(tr -dc a-zA-Z0-9 < /dev/urandom | head -c 10)
+content_random=$(tr -dc a-zA-Z0-9 < /dev/urandom | head -c 10)
 
-curl -X POST -Fcontent=$content_name -Fname=$content_random http://localhost:62222/test
+curl -X POST \
+-H "Content-Type: application/json" \
+--data "{ \"name\": \"$content_name\", \"content\": \"$content_random\" }" \
+http://localhost:62222/test
